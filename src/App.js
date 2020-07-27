77ce5183d494f0ea4c9ce4e99b1dcf126242ccc0
Santiago Fajardo <s.fajardot@uniandes.edu.co>
2020-07-26 21:05:51 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Particles from "react-particles-js";
-import Clarifai from 'clarifai';
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
@@ -10,10 +9,6 @@ import SignIn from "./components/SignIn/SignIn";
 import Register from "./components/Register/Register";
 import './App.css';
 
-
-const app = new Clarifai.App({
-    apiKey: '6049e8839f0f460683a597fe837ac23d'
-});
 
 const particlesOptions = {
     particles: {
@@ -27,23 +22,25 @@ const particlesOptions = {
     }
 }
 
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+    }
+}
+
 class App extends React.Component {
     constructor() {
         super();
-        this.state = {
-            input: '',
-            imageUrl: '',
-            box: {},
-            route: 'signin',
-            isSignedIn: false,
-            user: {
-                id: '',
-                name: '',
-                email: '',
-                entries: 0,
-                joined: ''
-            }
-        }
+        this.state = initialState;
     }
 
     loadUser = (data) => {
@@ -81,10 +78,17 @@ class App extends React.Component {
 
     onButtonSubmit = () => {
         this.setState({imageUrl: this.state.input})
-        app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+        fetch(' https://tranquil-everglades-16185.herokuapp.com/imageurl', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                input: this.state.input
+            })
+        })
+            .then(response => response.json())
             .then(response => {
-                if(response){
-                    fetch('http://localhost:3000/image', {
+                if (response) {
+                    fetch(' https://tranquil-everglades-16185.herokuapp.com/image', {
                         method: 'put',
                         headers: {'Content-Type': 'application/json'},
                         body: JSON.stringify({
@@ -93,8 +97,9 @@ class App extends React.Component {
                     })
                         .then(response => response.json())
                         .then(count => {
-                            this.setState(Object.assign(this.state.user, { entries: count}))
+                            this.setState(Object.assign(this.state.user, {entries: count}))
                         })
+                        .catch(console.log)
                 }
                 this.displayFaceBox(this.calculateFaceLocation(response))
             })
@@ -103,7 +108,7 @@ class App extends React.Component {
 
     onRouteChange = (route) => {
         if (route === 'signout') {
-            this.setState({isSignedIn: false})
+            this.setState(initialState)
         } else if (route === 'home') {
             this.setState({isSignedIn: true})
         }
